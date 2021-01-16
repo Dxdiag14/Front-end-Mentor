064f1dae877e619e6cd4969a4ea9753b1d1cf27c
HJ Miranda <hjmiranda02@gmail.com>
2021-01-16 22:48:52 +0800

    fading effect on li

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const links = document.querySelectorAll(".navigation li");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
 });
